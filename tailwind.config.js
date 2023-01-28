0aa4c0a756058f414e2a4fe64c38b7754a7c5da2
d3oxy <ishansachu1@gmail.com>
2023-01-28 17:42:05 +0000

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'Inter': ['Inter', 'sans-serif'],
+      'Poppins': ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('/hero-section.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
+  },
+  plugins: [],
+}

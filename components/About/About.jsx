079a0afbcebf7bf9c09ac8089f8deeab3206ce53
d3oxy <ishansachu1@gmail.com>
2023-01-29 15:22:55 +0000

    About

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -9,7 +9,9 @@ function About() {
                     <h2 className='font-Poppins font-bold text-underline'>About Us</h2>
                     <p className='mt-4 mb-0 font-Poppins font-normal '>Trip Valley is a service Travel Agency Based in Kerala since 2020. Dedicated in Providing Totally reliable and practical Travel Solution as it offers most Competitive Rates in the Market. We provide Resort booking & Tour Pacakg. Wayanad Munnar vagamon Ooty Kulukkumala Trekking Pacakge available for Manali, Kashmir, Goa, Maldives, Andaman Nicobar, Lakshadweep. Our mission is to provide the Greatest Product and services to the utmost satisfaction for all Agents as they are and will always be our number one Priority.</p>
                     <p className='font-Inter font-semibold pb-12'>Travel Agency Located Trikaripur, near Payyannur, Kannur.</p>
-                    <a className='bg-[#08898F] pr-14 pl-14 pt-4 pb-4 rounded-lg text-white font-Inter font-medium' href="/">Book Now</a>
+                    <div className='flex justify-center md:justify-start'>
+                        <a className='button bg-[#08898F] pr-14 pl-14 pt-4 pb-4 rounded-lg font-Inter font-medium' href="/">Book Now</a>
+                    </div>
                     {/* </div> */}
                 </div>
 

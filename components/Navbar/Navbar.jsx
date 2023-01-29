d740493e5b0453e8f34e8c1f8a94da8c9abbc83a
d3oxy <ishansachu1@gmail.com>
2023-01-29 11:39:30 +0000

    New Navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,46 +1,45 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { BiMenuAltRight } from 'react-icons/bi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 function NavbarComponent() {
+    const [toggle, setToggle] = useState(false)
     return (
-        <Navbar collapseOnSelect expand="lg">
-            <Container>
-                <Navbar.Brand href="#home" className=''>
-                    <div className='flex flex-row justify-center items-center'>
-                        <img src="/nav-logo.svg" alt="Logo" />
-                        <p className='mb-0 font-Poppins font-extrabold text-[#1A3724] uppercase'>Trip <span className='text-[#08898F]' >Valley</span></p>
-                    </div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        {/* <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown> */}
-                    </Nav>
-                    <Nav className=' flex flex-row justify-center items-center '>
-                        <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#home">Home</a>
-                        <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#">Packages</a>
-                        <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#">Resorts</a>
-                        <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#about">About Us</a>
-                        <a className=' font-Poppins font-normal text-[#FFFFFF] m-[5px] bg-[#08898F] p-2 rounded ' href="#">Contact Us</a>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <div className='w-full h-20 bg-white pl-2 md:pl-24 pr-2 md:pr-24 flex flex-row items-center'>
+                <div className=' w-[90%] md:w-[50%] flex flex-row items-center'>
+                    <img src="/nav-logo.svg" alt="Trip Valley Logo" />
+                    <p className='mb-0 text-2xl font-Poppins font-extrabold text-[#1A3724] uppercase'>Trip <span className='text-[#08898F]' >Valley</span></p>
+                </div>
+                <div className='w-[50%] flex md:hidden  justify-end items-center'>
+                    {toggle ? <AiOutlineClose className='text-black text-4xl block md:hidden' onClick={() => { setToggle(!toggle) }} /> : <BiMenuAltRight className='text-black text-4xl block md:hidden' onClick={() => { setToggle(!toggle) }} />}
+                </div>
+                <div className=' w-[10%] md:w-[50%] hidden md:flex  justify-end items-center'>
+                    <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#home">Home</a>
+                    <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#">Packages</a>
+                    <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#">Resorts</a>
+                    <a className=' font-Poppins font-normal text-[#000000] m-[5px] ' href="#about">About Us</a>
+                    <a className=' font-Poppins font-normal text-[#FFFFFF] m-[5px] bg-[#08898F] p-2 rounded ' href="#">Contact Us</a>
+                </div>
+                <div></div>
+            </div>
+            {toggle && <DropDown />}
+        </>
     )
 }
 
-export default NavbarComponent   
+
+export default NavbarComponent
+
+
+function DropDown() {
+    return (
+        <div className='w-full bg-white ease-in-out'>
+            hi
+        </div>
+    )
+}

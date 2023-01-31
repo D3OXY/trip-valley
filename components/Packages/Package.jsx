fd8459981fa2b37665f9ebf63a32b3cb21637fa1
d3oxy <ishansachu1@gmail.com>
2023-01-31 08:48:17 +0000

    explore

--- components/Packages/Package.jsx
+++ components/Packages/Package.jsx
@@ -55,9 +55,13 @@ function Package() {
                             <Card key={id} title={title} image={image} />
                         ))}
 
-                        <div className="object-fill bg-cover bg-[url('/card-bg.jfif')] flex items-center justify-center flex-col bg-white max-w-[300px] min-h-[156px] h-[100%] rounded-lg border-2 hover:border-4 hover:border-[#08898F] border-black cursor-pointer ease-in-out duration-200">
-                            <div className="flex items-center justify-center font-Poppins font-extrabold text-xl">
-                                <p className="text-black mt-4 mb-0 ">Explore All</p>
+                        <div className="card bg-black">
+                            <div className="card_image min-h-[156px] max-h-[156px]">
+                                <img src="/card-bg.jfif" alt="Explore All" />
+                                <div class="absolute inset-0 bg-black opacity-30"></div>
+                                <div className="absolute inset-0 card_title text-white flex flex-row justify-end items-end pr-4 font-Poppins font-bold text-xl opacity-100">
+                                    <p>Explore All</p>
+                                </div>
                             </div>
                         </div>
                     </div>

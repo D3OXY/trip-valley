e893f7a929f93d53588409fb4110ae1dccdaaa79
d3oxy <ishansachu1@gmail.com>
2023-01-31 17:30:58 +0000

    Update

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,5 +1,5 @@
 
-function Card({ id, image, title }) {
+function Card({ id, image, name }) {
     return (
         // <article key={id} className="flex items-center justify-center flex-col bg-white max-w-[300px] min-h-[156px] h-[100%] p-2 rounded-lg border-2 hover:border-4 hover:border-[#08898F] border-black cursor-pointer ease-in-out duration-200">
         //     {image && (
@@ -15,10 +15,10 @@ function Card({ id, image, title }) {
 
         <div className="card bg-black">
             <div className="card_image min-h-[156px] max-h-[156px]">
-                <img src={image} alt={title} />
+                <img src={image} alt={name} />
                 <div class="absolute inset-0 bg-black opacity-30"></div>
                 <div className="absolute inset-0 card_title text-white flex flex-row justify-end items-end pr-4 font-Poppins font-bold text-xl opacity-100">
-                    <p>{title}</p>
+                    <p>{name}</p>
                 </div>
             </div>
         </div>

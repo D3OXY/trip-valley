dccce2c8e91ecd3977e4755eed99a91c63798df8
d3oxy <ishansachu1@gmail.com>
2023-02-01 16:33:26 +0000

    cleanup

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,17 +1,6 @@
 
 function Card({ id, image, name }) {
     return (
-        // <article key={id} className="flex items-center justify-center flex-col bg-white max-w-[300px] min-h-[156px] h-[100%] p-2 rounded-lg border-2 hover:border-4 hover:border-[#08898F] border-black cursor-pointer ease-in-out duration-200">
-        //     {image && (
-        //         <div className="max-h-[156px]  overflow-hidden object-fill rounded-lg">
-        //             <img src={image} alt={title} />
-        //         </div>
-        //     )}
-        //     <div className="flex items-center justify-center font-Poppins font-bold">
-        //         <p className="text-black mt-4 mb-0 ">{title}</p>
-        //     </div>
-        // </article>
-        // <div className="cards-list">
         <article key={id}>
             <div className="card bg-black">
                 <div className="card_image min-h-[156px] max-h-[156px]">
@@ -23,7 +12,6 @@ function Card({ id, image, name }) {
                 </div>
             </div>
         </article>
-        // </div>
     );
 }
 

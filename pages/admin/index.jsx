a21848b0d80831c277173a0493ccb3dbecff277c
d3oxy <ishansachu1@gmail.com>
2023-02-03 16:38:02 +0000

    logout

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import AuthCheck from '../../components/AuthCheck'
+import { useAuth } from '../../lib/AuthContext'
 
 function AdminDashboard() {
+    const { logout } = useAuth()
     return (
         <AuthCheck>
-            <div>AdminDashboard</div>
+            <div>
+                <h1>AdminDashboard</h1>
+                <button onClick={() => {
+                    logout();
+                }} >LogOut</button>
+            </div>
         </AuthCheck>
     )
 }

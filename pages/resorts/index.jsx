44f912642b07dedaec3fa22692acd3aa66399d13
d3oxy <ishansachu1@gmail.com>
2023-02-03 05:58:03 +0000

    resort page

--- pages/resorts/index.jsx
+++ pages/resorts/index.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from '../../components/Footer/Footer'
+import NavbarComponent from '../../components/Navbar/Navbar'
+import Card from '../../components/Card'
+import data from '../../components/data.json'
+import WhatsAppIcon from '../../components/WhatsAppIcon'
+import styles from './Card.module.css'
 
 function Resorts() {
     return (
-        <div>Resorts</div>
+        <>
+            <NavbarComponent />
+            <section>
+                <div className='hero__alternative w-[100%] h-[500px]'>
+                    <div className='w-[100%] md:w-[50%] h-[100%] flex flex-col pt-[100px] md:pt-0 md:justify-center items-center'>
+                        < h1 className='text-black uppercase font-Poppins font-semibold text-5xl' >Resorts</h1 >
+                        <p className='text-black mb-4 md:mb-0 font-Poppins font-normal'>Our Resorts</p>
+                    </div >
+                    <div className='w-[50%] h-[100%] hidden '></div>
+                </div >
+            </section>
+            <Container>
+                <section id='#resort'>
+                    <div className='mt-20 mb-20'>
+                        <div className='flex justify-center items-center'>
+                            <div className="card__container">
+                                {data.resorts?.map(({ name, image }, id) => (
+                                    <Card key={id} name={name} image={image} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </Container>
+            <Footer />
+            <WhatsAppIcon />
+        </>
     )
 }
 

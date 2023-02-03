56a1c48cd82fab55d38dfdd8f7cce03d1bd05dab
d3oxy <ishansachu1@gmail.com>
2023-02-03 05:57:34 +0000

    New Card Design

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 
 function Card({ image, name }) {
+    const [hover, setHover] = useState(false);
     return (
-        <article>
-            <div className="card bg-black">
-                <div className="card_image min-h-[156px] max-h-[156px]">
-                    <img src={image} alt={name} />
-                    <div className="absolute inset-0 bg-black opacity-30"></div>
-                    <div className="absolute inset-0 card_title text-white flex flex-row justify-end items-end pr-4 font-Poppins font-bold text-xl opacity-100">
-                        <p>{name}</p>
-                    </div>
-                </div>
+        <>
+            <div
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                style={{
+                    width: "300px",
+                    height: "200px",
+                    background: `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${image})`,
+                    backgroundSize: "cover",
+                    borderRadius: "15px",
+                    border: "2px solid black",
+                    boxShadow: !hover ? "0 0 10px #ddd" : "0 0 10px black",
+                    display: "flex",
+                    alignItems: "flex-end",
+                    justifyContent: "flex-end",
+                    padding: "10px",
+                    transform: hover ? "scale(0.9)" : "scale(1)",
+                    transition: "transform 0.3s ease-in-out"
+                }}>
+                <p className="m-0 font-Poppins font-bold text-xl text-white">{name}</p>
             </div>
-        </article>
+        </>
     );
 }
 

284753b06674a4c2c31b6679ed9fdc0af7b1cf63
d3oxy <ishansachu1@gmail.com>
2023-02-04 19:13:49 +0000

    page load progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import { AuthContextProvider } from '../lib/AuthContext'
 import '../styles/globals.css'
+import "../styles/nprogress.css"
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import dynamic from 'next/dynamic';
+
+const TopProgressBar = dynamic(
+  () => {
+    return import("../components/TopProgressBar");
+  },
+  { ssr: false },
+);
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <AuthContextProvider>
+        <TopProgressBar />
         <Component {...pageProps} />
       </AuthContextProvider>
     </>

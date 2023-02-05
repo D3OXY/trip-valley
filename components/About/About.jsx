0db6cc556d9264893f9303a42a7af50f910cb846
d3oxy <ishansachu1@gmail.com>
2023-02-05 04:41:53 +0000

    spell fix

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
             <div className='mt-20 mb-20 flex flex-col md:flex-row'>
                 <div className='w-[100%] md:w-[50%]' >
                     <h2 className='font-Poppins font-bold text-underline'>About Us</h2>
-                    <p className='mt-4 mb-0 font-Poppins font-normal '>Trip Valley is a service Travel Agency Based in Kerala since 2020. Dedicated in Providing Totally reliable and practical Travel Solution as it offers most Competitive Rates in the Market. We provide Resort booking & Tour Pacakg. Wayanad Munnar vagamon Ooty Kulukkumala Trekking Pacakge available for Manali, Kashmir, Goa, Maldives, Andaman Nicobar, Lakshadweep. Our mission is to provide the Greatest Product and services to the utmost satisfaction for all Agents as they are and will always be our number one Priority.</p>
+                    <p className='mt-4 mb-0 font-Poppins font-normal '>Trip Valley is a service Travel Agency Based in Kerala since 2020. Dedicated in Providing Totally reliable and practical Travel Solution as it offers most Competitive Rates in the Market. We provide Resort booking & Tour Package. Wayanad Munnar Vagamon Ooty Kulukkumala Trucking Package available for Manali, Kashmir, Goa, Maldives, Andaman Nicobar, Lakshadweep. Our mission is to provide the Greatest Product and services to the utmost satisfaction for all Agents as they are and will always be our number one Priority.</p>
                     <p className='font-Inter font-semibold pb-12'>Travel Agency Located Trikaripur, near Payyannur, Kannur.</p>
                     <div className='flex justify-center md:justify-start'>
                         <a className='button bg-[#08898F] pr-14 pl-14 pt-4 pb-4 rounded-lg font-Inter font-medium' href="/">Book Now</a>

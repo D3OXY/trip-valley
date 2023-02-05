0eddbd06fad7d692b6a7bb80197759ae0a8f30b4
d3oxy <ishansachu1@gmail.com>
2023-02-05 04:32:23 +0000

    cleanup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/Navbar/Navbar'
-import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hero from '../components/Hero/Hero';
-import About from '../components/About/About';
 import { Container } from 'react-bootstrap';
-import TwoCardRow from '../components/Card';
-import WhatsAppIcon from '../components/WhatsAppIcon';
-import Resort from '../components/Resorts/Resort';
-import Package from '../components/Packages/Package';
+import About from '../components/About/About';
 import Footer from '../components/Footer/Footer';
-const cards = [
-  {
-    title: 'Card title',
-    text: 'Some quick example text to build on the card title and make up the bulk of the card content.',
-  },
-  {
-    title: 'Card title',
-    text: 'Some quick example text to build on the card title and make up the bulk of the card content.',
-  },
-]
+import Hero from '../components/Hero/Hero';
+import Navbar from '../components/Navbar/Navbar';
+import Package from '../components/Packages/Package';
+import Resort from '../components/Resorts/Resort';
+import WhatsAppIcon from '../components/WhatsAppIcon';
 
 export default function Home() {
   return (
@@ -31,7 +17,6 @@ export default function Home() {
         <About />
         <Resort />
         <Package />
-        {/* <TwoCardRow cards={cards} /> */}
       </Container>
       <Footer />
       <WhatsAppIcon />

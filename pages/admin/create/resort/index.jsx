c68534388aebf54e54a34f7511f2d728ba194e15
d3oxy <ishansachu1@gmail.com>
2023-02-05 04:28:12 +0000

    cleanup

--- pages/admin/create/resort/index.jsx
+++ pages/admin/create/resort/index.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import AuthCheck from '../../../../components/AuthCheck'
-import NavbarComponent from '../../../../components/Navbar/Navbar'
-import Footer from '../../../../components/Footer/Footer'
+import { addDoc, collection, getDocs, query, Timestamp, where } from 'firebase/firestore';
 import { useRouter } from "next/router";
-import IncludesForm from "./IncludesForm";
+import React, { useState } from "react";
+import AuthCheck from '../../../../components/AuthCheck';
+import Footer from '../../../../components/Footer/Footer';
+import NavbarComponent from '../../../../components/Navbar/Navbar';
+import UploadFile from "../../../../components/UploadFile";
+import { db, postToJSON } from "../../../../lib/firebase";
 import DataForm from "./DataForm";
 import ImageForm from "./ImageForm";
+import IncludesForm from "./IncludesForm";
 import PricesForm from "./PricesForm";
-import UploadFile from "../../../../components/UploadFile";
-import { db, postToJSON } from "../../../../lib/firebase"
-import { collection, addDoc, Timestamp, query, where, getDocs } from 'firebase/firestore'
 const defaultData = {
     name: '',
     description: '',

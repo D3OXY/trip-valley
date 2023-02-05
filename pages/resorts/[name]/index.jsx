544f87761fd43390213cf7cc1b499a40e45bc9a4
d3oxy <ishansachu1@gmail.com>
2023-02-05 04:16:29 +0000

    dynamic resorts

--- pages/resorts/[name]/index.jsx
+++ pages/resorts/[name]/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import Footer from '../../../components/Footer/Footer'
 import NavbarComponent from '../../../components/Navbar/Navbar'
 import WhatsAppIcon from '../../../components/WhatsAppIcon'
 import data from '../../../components/data.json'
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore'
+import { db, postToJSON } from '../../../lib/firebase'
 
 
 export async function getStaticPaths() {
-    const resorts = data.resorts
-    if (resorts) {
+    const q = query(collection(db, "resorts"), orderBy("createdAt", "asc"));
+    const resorts = await (await getDocs(q)).docs.map(postToJSON);
+    if (resorts.length) {
         const paths = resorts.map((resort) => ({
             params: { name: resort.name.toLowerCase() },
         }))
@@ -22,15 +25,12 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     const name = params.name.toLowerCase();
+    const q = query(collection(db, "resorts"), where("name", "==", name));
+    const data = await (await getDocs(q)).docs.map(postToJSON);
 
-    //check if data has the resortname
-    const resort = data.resorts.find((resort) => resort.name.toLowerCase() === name);
-    if (resort) {
+    if (data.length) {
         return {
-            props: {
-                name: resort.name,
-                image: resort.image,
-            }
+            props: { data }
         }
     } else {
         return {
@@ -44,16 +44,18 @@ export async function getStaticProps({ params }) {
 
 
 function Index(props) {
+    const [data, setData] = useState(props.data[0]);
+    let name = data.name.replace(/-/g, ' ');
+    name = name.charAt(0).toUpperCase() + name.slice(1);
     return (
         <>
             <NavbarComponent />
             <section>
-                <div className='hero__alternative w-[100%] h-[500px]'>
-                    <div className='w-[100%] md:w-[50%] h-[100%] flex flex-col pt-[100px] md:pt-0 md:justify-center items-center'>
-                        < h1 className='text-black uppercase font-Poppins font-semibold text-5xl' >{props.name}</h1 >
-                        <p className='text-black mb-4 md:mb-0 font-Poppins font-normal'>{props.name} Resort Details</p>
+                <div className='hero__alternative w-[100%] h-[500px] flex items-center justify-center'>
+                    <div className='w-full h-[100%] flex flex-col justify-center items-center'>
+                        < h1 className='text-black font-Poppins font-extrabold text-2xl md:text-5xl text-center' >{name}</h1 >
+                        <p className='text-black mb-4 md:mb-0 font-Poppins font-normal'>Resort</p>
                     </div >
-                    <div className='w-[50%] h-[100%] hidden '></div>
                 </div >
             </section>
             <Container>
@@ -62,18 +64,17 @@ function Index(props) {
                     <div className='min-w-[90vw] md:min-w-[50vw] max-w-[90vw] md:max-w-[50vw] mb-12'>
 
                         <div className='min-w-[90vw] md:min-w-[50vw] border-2 border-black p-8'>
-                            <div className='mb-4'>
-                                <div className='flex flex-row border-2 border-black'>
-                                    <div className='w-[50%] border-r-2 border-black flex items-center justify-center font-Poppins font-semibold p-2'>For Bachelors</div>
-                                    <div className='w-[50%] border-l-2 border-black flex items-center justify-center font-Poppins font-semibold p-2'>650/-</div>
-                                </div>
-                            </div>
-                            <div className='mt-4'>
-                                <div className='flex flex-row border-2 border-black'>
-                                    <div className='w-[50%] border-r-2 border-black flex items-center justify-center font-Poppins font-semibold p-2'>For Couples</div>
-                                    <div className='w-[50%] border-l-2 border-black flex items-center justify-center font-Poppins font-semibold p-2'>1999/-</div>
-                                </div>
-                            </div>
+                            {data.prices.map((price, index) => {
+                                return (
+                                    <div key={index} className='mb-4'>
+                                        <div className='flex flex-row border-2 border-black'>
+                                            <div className='w-[50%] border-r-2 border-black flex items-center justify-center font-Poppins font-semibold p-2'>{price.name}</div>
+                                            <div className='w-[50%] border-l-2 border-black flex items-center justify-center font-Poppins font-semibold p-2'>{price.amount}</div>
+                                        </div>
+                                    </div>
+                                )
+
+                            })}
                         </div>
                         <div className='flex flex-col md:flex-row items-center justify-center mt-4'>
                             <div className='w-[100%] md:w-[50%] flex items-center justify-center'>
@@ -81,13 +82,13 @@ function Index(props) {
                                     <div className='flex flex-row m-4'>
                                         <div className='w-[50%] font-Poppins font-semibold text-white '>Check In</div>
                                         <div className='w-[50%] flex justify-end ' >
-                                            <div className='font-Poppins font-semibold text-white'>10:00 AM</div>
+                                            <div className='font-Poppins font-semibold text-white'>{data.checkin}</div>
                                         </div>
                                     </div>
                                     <div className='flex flex-row m-4'>
                                         <div className='w-[50%] font-Poppins font-semibold text-white '>Check Out</div>
                                         <div className='w-[50%] flex justify-end ' >
-                                            <div className='font-Poppins font-semibold text-white '>1:00 AM</div>
+                                            <div className='font-Poppins font-semibold text-white '>{data.checkout}</div>
                                         </div>
                                     </div>
                                 </div>
@@ -95,15 +96,16 @@ function Index(props) {
                             <div className='w-[100%] md:w-[50%] flex flex-col items-center justify-center '>
                                 <h2 className='font-Poppins font-bold '>Includes</h2>
                                 <ul className='font-Poppins font-semibold '>
-                                    <li>▶ Breakfast</li>
-                                    <li>▶ Pool</li>
-                                    <li>▶ Room</li>
-                                    <li>▶ Games</li>
+                                    {data.includes.map((include, index) => {
+                                        return (
+                                            <li key={index}>▶ {include}</li>
+                                        )
+                                    })}
                                 </ul>
                             </div>
                         </div>
-                        <div className='font-Inter font-semibold m-4'>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, recusandae delectus voluptatem illum eveniet nisi similique odio quo inventore a quidem beatae magnam vero officiis qui. Cum doloremque provident hic.
+                        <div className='font-Inter font-semibold m-4 text-center'>
+                            {data.description}
                         </div>
 
                     </div>
@@ -111,13 +113,11 @@ function Index(props) {
                     <div className='flex flex-col items-start justify-start w-[90vw] md:w-[70vw]'>
                         <h1 className='text-underline font-Poppins font-bold text-2xl'>Images</h1>
                         <div className='mt-6 flex flex-col items-center justify-center w-[90vw] md:w-[70vw] mb-12'>
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
-                            <img src="/placeholder-img.jfif" alt="fffg" className='max-w-[80vw] md:max-w-[60vw] mt-4' />
+                            {data.images.map((image, index) => {
+                                return (
+                                    <img key={index} src={image} alt={name} className='max-w-[80vw] md:max-w-[60vw] mt-4' />
+                                )
+                            })}
                         </div>
                     </div>
 
